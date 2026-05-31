d7e12780064f013c55592deee9c770e67f7398a8
Herambha1226 <pallapothugayathri23@gmail.com>
2026-05-31 20:28:34 +0530

    update database

--- app/main.py
+++ app/main.py
@@ -25,11 +25,12 @@ app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     }
 }
 app.config["MAIL_SERVER"] = os.getenv("EMAIL_SERVER")
-app.config["MAIL_PORT"]           = 465     
-app.config["MAIL_USE_TLS"]        = False    
-app.config["MAIL_USE_SSL"]        = True  
+app.config["MAIL_PORT"]           = 587  
+app.config["MAIL_USE_TLS"]        = True    
+app.config["MAIL_USE_SSL"]        = False  
 app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASSWORD")
+app.config["MAIL_PASSWORD"] = os.getenv("BREVO_SMTP_KEY")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("EMAIL_USER")
 
 db.init_app(app)
 login_manager.init_app(app)
